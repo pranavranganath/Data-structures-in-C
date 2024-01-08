f7e18f9202d8e2e1b5b2dc0e63ceb58c851632a7
pranavranganath <155946238+pranavranganath@users.noreply.github.com>
2024-01-08 12:20:09 +0530

    Add files via upload

---
+++ functions of stack using c.c
@@ -0,0 +1,100 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 4
+
+int top = -1, inp_array[SIZE];
+void push();
+void pop();
+void show();
+
+
+void main(){
+
+
+    int choice;
+
+
+    while(1){
+
+            printf("\nPerform operations on the stack:");
+        printf("\n1.Push the element\n2.Pop the element\n3.Show\n4.End");
+        printf("\n\nEnter the choice: ");
+        scanf("%d", &choice);
+        switch (choice){
+            case 1:
+            push();
+            break;
+
+            case 2:
+            pop();
+            break;
+
+            case 3:
+            show();
+            break;
+
+            case 4:
+            exit(0);
+
+
+            default:
+            printf("\n Invalid choice!\n");
+
+
+
+        }
+
+    }
+}
+
+
+void push()
+{
+    int x;
+
+    if(top == SIZE-1)
+    {
+        printf("/n stackoverflow/\n");
+    }
+
+    else{
+        printf("\nEnter the element to be added in the stack.\n");
+        scanf("%d",&x);
+        top=top+1;
+        inp_array[top]=x;
+    }
+}
+
+
+void pop(){
+
+    if(top==-1){
+        printf("StackUnderflow");
+
+    }
+    else{
+        printf("\nPopped element: %d", inp_array[top]);
+         top=top-1;
+    }
+}
+
+
+void show(){
+
+   if(top==-1){
+    printf("/n stackUnderflow");
+
+   }
+   else{
+    printf("\n Elements present in the stack\n");
+    for (int i=top;i>=0;--i){
+        printf("%d\n",inp_array[i]);
+    }
+
+
+   }
+
+}
+
